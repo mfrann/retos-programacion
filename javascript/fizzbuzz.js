1034ebea62346c4eac5d54cb30a757e3867cfffd
Fran <mfranfc@gmail.com>
2025-11-21 12:35:14 -0500

    primer reto completado - fizzbuzz

--- javascript/fizzbuzz.js
+++ javascript/fizzbuzz.js
@@ -7,4 +7,16 @@
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  */
 
-for (var i = 0; i < 100; i++) {}
+for (var i = 0; i < 100; i++) {
+  //variable - condicion - aumento
+  if (i % 3 === 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  } else if (i % 15 === 0) {
+    //SACAMOS MCM DE 3 Y 5
+    console.log("fizzbuzz");
+  } else {
+    console.log(i);
+  }
+}
