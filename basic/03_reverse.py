fc08302686a9b884b423c06429d7311462e1aa64
Fran <mfranfc@gmail.com>
2025-12-31 12:09:46 -0500

    Reto #7: COMPLETADO

---
+++ basic/03_reverse.py
@@ -0,0 +1,13 @@
+'''
+#TODO: 
+/*
+ * Crea un programa que invierta el orden de una cadena de texto
+ * sin usar funciones propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+*/
+'''
+
+def reverse_sym(word):
+    return word[::-1]
+
+print(reverse_sym('Hola Mundo'))
